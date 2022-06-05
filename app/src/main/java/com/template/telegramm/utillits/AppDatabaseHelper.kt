589860080ef1929e4886a8c4bf26ab7a7031dce8
rustam <r.kozhinov@mail.ru>
2022-06-05 15:57:44 +1000

    when the user enters to ContactsFragment, he will see a list of all his contacts

--- app/src/main/java/com/template/telegramm/utillits/AppDatabaseHelper.kt
+++ app/src/main/java/com/template/telegramm/utillits/AppDatabaseHelper.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.net.Uri
 import android.provider.ContactsContract
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -126,5 +127,8 @@ fun updatePhoneToDatabase(arrayContacts: ArrayList<CommandModel>) {
         }
     })
 }
+
+fun DataSnapshot.getCommandModel(): CommandModel =
+    this.getValue(CommandModel::class.java) ?: CommandModel()
 
 
